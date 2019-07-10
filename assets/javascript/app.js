09328d44bf5040f42e718073e1fd283306862b7b
John Mateer <jj.mateer@live.com>
2019-07-10 15:34:26 -0700

    Made a container with divs on the html page and positioned them with css according to the layout we were given. In JS, I created variables for time remaining, user choices, current question, and to count the answers. I also  made individual variables for the questions that will be displayed and the choices that match. Did some basic pseudo coding.

---
+++ assets/javascript/app.js
@@ -0,0 +1,72 @@
+//Create Variables
+var timeRemaining = 30000;
+var currentQuestion;
+var userChoices;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+//Create reset function
+function reset() {
+
+}
+//make an onLoad function that dynamically creates a start button
+    var startBtn = document.getElementById("#startBtn");
+    
+
+//Make variables for the questions
+var question1 = "What fast food chain sells the whopper?"
+var question2 = "What fast food chain sells the big mac?"
+var question3 = "What fast food chain sells the triple layer nacho?"
+var question4 = "What fast food chain sells animal fries?"
+var question5 = "What fast food chain sells kentucky fried chicken?"
+
+//Make arrays for each question containing their choices
+var question1Choices = ["Mcdonald's", "Carl's Jr.", "Burger King", "Taco bell"];
+var question2Choices = ["Mcdonald's", "Taco bell", "Burger King", "Carl's Jr."];
+var question3Choices = ["Burger King", "Carl's Jr.","Mcdonald's", "Taco bell"];
+var question4Choices = ["In-n-out", "Burger King", "Carl's Jr.", "Taco bell"];
+var question5Choices = ["Carl's Jr.", "Mcdonald's", "KFC", "Taco bell"];
+//Create on click function for the button to start the game
+
+//start timer counting down from 30 seconds
+setTimeout(function () { alert("Hello"); }, 5000);
+
+//display the first question to triviaQuestion div
+
+
+
+// When user choice is clicked, 
+
+
+
+ //if guessed correctly
+
+ //stop timer and display how much time is left
+
+    //tell the user they are correct
+
+    //display the gif of the correct answer
+
+//if guessed incorrectly
+
+//stop timer and display how much time is left
+
+    //tell the user they are incorrect
+
+    //display the correct answer with gif underneath
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
