160557841906d92e175a3ebe55984fb34f55685a
John Mateer <jj.mateer@live.com>
2019-07-11 17:15:39 -0700

    Created variables for all the user choices with the properties Name and a list of other variables I made(q1-5) that determine if it is the right answer for the current question. I set the values of q1-5 to true || false. Added if else statements to the inside of the #choice1-4 click functions. If q1 is true, alert correct. If q1 is false, alert incorrect.Each onclick function will set the value of q1 to either true or false.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,8 @@ var userChoices;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
+var correct = true;
+var incorrect = false;
 
 //Create reset function
 function reset() {
@@ -13,37 +15,40 @@ function reset() {
 //make an onLoad function that dynamically creates a start button
 var startBtn = document.getElementById("startBtn");
 
-//Make variables for the questions
-var question1 = "What fast food chain sells the whopper?"
-var question2 = "What fast food chain sells the big mac?"
-var question3 = "What fast food chain sells the triple layer nacho?"
-var question4 = "What fast food chain sells animal fries?"
-var question5 = "What fast food chain sells kentucky fried chicken?"
+//Make variables to display the questions
+var question1 = "What fast food chain sells the whopper?";
+var question2 = "What fast food chain sells the big mac?";
+var question3 = "What fast food chain sells the triple layer nacho?";
+var question4 = "What fast food chain sells animal fries?";
+var question5 = "What fast food chain sells kentucky fried chicken?";
 
-1
 
+// { answer: "panda express", correct: false }];
+//Create variables for each choice and make them true or false for specific questions
+var mcdonalds = {Name:"Mcdonald's", q1:false, q2:true, q3:false, q4:false, q5:false};
+var carlsJr = {Name:"Carl's Jr.", q1:false, q2:false, q3:false, q4:false, q5:false};
+var burgerKing = {Name:"Burger King", q1:true, q2:false, q3:false, q4:false, q5:false};
+var tacoBell = {Name:"Taco Bell", q1:false, q2:false, q3:true, q4:false, q5:false};
+var inNOut= {Name:"In-n-out", q1:false, q2:false, q3:false, q4:true, q5:false};
+var kfc = {Name:"KFC", q1:false, q2:false, q3:false, q4:false, q5:true};
+//link questions
+var q1 = false || true;
+var q2 = false || true;
+var q3 = false || true;
+var q4 = false || true;
+var q5 = false || true;
 
 //Make arrays for each question containing their choices
-var question1Choices = ["Mcdonald's", "Carl's Jr.", "Burger King", "Taco bell", { answer: "panda express", correct: false }];
-var question2Choices = ["Mcdonald's", "Taco bell", "Burger King", "Carl's Jr."];
-var question3Choices = ["Burger King", "Carl's Jr.", "Mcdonald's", "Taco bell"];
-var question4Choices = ["In-n-out", "Burger King", "Carl's Jr.", "Taco bell"];
-var question5Choices = ["Carl's Jr.", "Mcdonald's", "KFC", "Taco bell"];
+// var question1Choices = [mcdonalds, carlsJr, burgerKing, tacoBell];
+// var question2Choices = [mcdonalds, tacoBell, burgerKing, carlsJr];
+// var question3Choices = [burgerKing, carlsJr, mcdonalds, tacoBell];
+// var question4Choices = [inNOut, burgerKing, carlsJr, tacoBell];
+// var question5Choices = [carlsJr, mcdonalds, kfc, tacoBell];
 
-//Make variables for correct answers
-var correctAnswer1 = question1Choices[2];
-var correctAnswer2 = question2Choices[0];
-var correctAnswer3 = question3Choices[3];
-var correctAnswer4 = question4Choices[0];
-var correctAnswer5 = question5Choices[2];
+
 //Create on click function for the button to start the game
 startBtn.addEventListener("click", function () {
-    
-
 });
-//start timer counting down from 30 seconds
-
-
 
 //display the first question to triviaQuestion div
 $(startBtn).click(function () {
@@ -55,16 +60,51 @@ $(startBtn).click(function () {
             $("#timeRemaining").html("Time remaining: " + i);
             i--;
         }, 1000);
-        
+        $("#startButton").empty();
     });
-     
-    // $("#resetButton").click(function (e) {
-    //     i = 30;
-    // });
 
     //display question1 choices
-    $("#userChoices").html(question1Choices);
-    
+    $("#choice1").text(mcdonalds.Name);
+    $("#choice2").text(carlsJr.Name);
+    $("#choice3").text(burgerKing.Name);
+    $("#choice4").text(tacoBell.Name);
+
+    //if else statements to alert correct or incorrect
+    $("#choice1").click(function () {
+        //sets value of q1 to determine if the answer is right or wrong
+        q1 = false;
+        //If q1 is true alert "correct!!", if false alert "incorrect!!"
+        if(q1 === true) {
+            alert("correct!!")
+        } else if(q1 === false) {
+            alert("incorrect!!")
+        }
+    });
+    $("#choice2").click(function () {
+        q1 = false;
+
+        if(q1 === true) {
+            alert("correct!!")
+        } else if(q1 === false) {
+            alert("incorrect!!")
+        }
+    });
+    $("#choice3").click(function () {
+        q1 = true;
+        if(q1 === true) {
+            alert("correct!!")
+        } else if(q1 === false) {
+            alert("incorrect!!")
+        }
+    });
+    $("#choice4").click(function () {
+        q1 = false;
+        if(q1 === true) {
+            alert("correct!!")
+        } else if(q1 === false) {
+            alert("incorrect!!")
+        }
+    });
 
 });
 
