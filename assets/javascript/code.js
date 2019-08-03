a5b0a8e664155e336b3224eed36394927d1a31f2
John Mateer <jj.mateer@live.com>
2019-08-02 21:06:14 -0700

    Added on click function for buttons in each question, made function to render question with their corresponding buttons, alert user of the correct answer, and go to the next question. They iterate through till the end and log correct answers. Timer does not reset after each question and it does not count incorrect answers yet.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -12,14 +12,21 @@ const questionArray = [
     q4 = "What fast food chain sells animal fries?",
     q5 = "What fast food chain sells kentucky fried chicken?"
 ];
+
+q1 = false;
+q2 = false;
+q3 = false;
+q4 = false;
+q5 = false;
 console.log(questionArray);
 //Choices
+
 var choicesArray = new Array();
-choicesArray[0] = ["<btn class ='cbtn'>burgerKing</btn>", "<btn class ='cbtn'>carlsJr</btn>", "<btn class ='cbtn'>mcdonalds</btn>", "<btn class ='cbtn'>tacoBell</btn>"];
-choicesArray[1] = ["<btn class ='cbtn'>mcdonalds</btn>", "<btn class ='cbtn'>tacoBell</btn>", "<btn class ='cbtn'>burgerKing</btn>", "carlsJr</btn>"];
-choicesArray[2] = ["<btn class ='cbtn'>burgerKing</btn>", "<btn class ='cbtn'>carlsJr</btn>", "<btn class ='cbtn'>mcdonalds</btn>", "<btn class ='cbtn'>tacoBell</btn>"];
-choicesArray[3] = ["<btn class ='cbtn'>inNOut</btn>", "<btn class ='cbtn'>burgerKing</btn>", "<btn class ='cbtn'>carlsJr</btn>", "<btn class ='cbtn'>tacoBell</btn>"];
-choicesArray[4] = ["<btn class ='cbtn'>carlsJr</btn>", "<btn class ='cbtn'>mcdonalds</btn>", "<btn class ='cbtn'>kfc</btn>", "<btn class ='cbtn'>tacoBell</btn>"];
+choicesArray[0] = ["<btn id='r' class='cbtn'>burgerKing</btn>", "<btn id='w' class='cbtn'>carlsJr</btn>", "<btn id='w' class='cbtn'>mcdonalds</btn>", "<btn id='w' class='cbtn'>tacoBell</btn>"];
+choicesArray[1] = ["<btn id='r' class='cbtn2'>mcdonalds</btn>", "<btn id='w' class='cbtn2'>tacoBell</btn>", "<btn id='w' class='cbtn2'>burgerKing</btn>", "<btn id='w' class='cbtn2'>carlsJr</btn>"];
+choicesArray[2] = ["<btn id='w' class='cbtn3'>burgerKing</btn>", "<btn id='w' class='cbtn3'>carlsJr</btn>", "<btn id='w' class='cbtn3'>mcdonalds</btn>", "<btn id='r' class='cbtn3'>tacoBell</btn>"];
+choicesArray[3] = ["<btn id='r' class='cbtn4'>inNOut</btn>", "<btn id='w' class='cbtn4'>burgerKing</btn>", "<btn id='w' class='cbtn4'>carlsJr</btn>", "<btn id='w' class='cbtn4'>tacoBell</btn>"];
+choicesArray[4] = ["<btn id='w' class='cbtn5'>carlsJr</btn>", "<btn id='w' class='cbtn5'>mcdonalds</btn>", "<btn id='r' class='cbtn5'>kfc</btn>", "<btn id='w' class='cbtn5'>tacoBell</btn>"];
 
 // Correct  choices
 var rightChoices = [
@@ -29,73 +36,40 @@ var rightChoices = [
     choicesArray[3][0],
     choicesArray[4][2]
 ];
+
 console.log(choicesArray);
 //Gifs for each correct answer
-// var qGifs = [
-//     $('#gifDiv').append('<img src="assets/images/burgerkinggif.webp" />'),
-//     $('#gifDiv').append('<img src="assets/images/mcdonaldsgif.webp" />'),
-//     $('#gifDiv').append('<img src="assets/images/tacoBellgif.webp" />'),
-//     $('#gifDiv').append('<img src="assets/images/inNoutgif.webp" />'),
-//     $('#gifDiv').append('<img src="assets/images/kfcgif.webp" />')
-// ];
+var qGifs = [
+    $('#gifDiv').html('<img src="assets/images/burgerkinggif.webp" />'),
+    $('#gifDiv').html('<img src="assets/images/mcdonaldsgif.webp" />'),
+    $('#gifDiv').html('<img src="assets/images/tacoBellgif.webp" />'),
+    $('#gifDiv').html('<img src="assets/images/inNoutgif.webp" />'),
+    $('#gifDiv').html('<img src="assets/images/kfcgif.webp" />')
+];
 
-// var qGifs = new Array();
-
-// qGifs[0] = new Image();
-// qGifs[0].src = '<img src="assets/images/burgerkinggif.webp" />';
-
-// qGifs[1] = new Image();
-// qGifs[1].src = '<img src="assets/images/mcdonaldsgif.webp />';
-
-// qGifs[2] = new Image();
-// qGifs[2].src = '<img src="assets/images/tacoBellgif.webp" />';
-
-// qGifs[3] = new Image();
-// qGifs[3].src = '<img src="assets/images/inNoutgif.webp" />';
-
-// qGifs[4] = new Image();
-// qGifs[4].src = '<img src="assets/images/kfcgif.webp" />';
-
-function nextImage(element) {
-    var img = document.getElementById(element);
-
-    for (var i = 0; i < qGifs.length; i++) {
-        if (qGifs[i].src == img.src) // << check this
-        {
-            if (i === qGifs.length) {
-                document.getElementById(element).src = qGifs[0].src;
-                break;
-            }
-            document.getElementById(element).src = qGifs[i + 1].src;
-            break;
-        }
-    }
-}
 
 //Text displaying the correct answer
-var qText = new Array();
-qText[0] = "Correct answer: Burger King",
-    qText[1] = "Correct answer: Mcdonald's",
-    qText[2] = "Correct answer: Taco Bell",
-    qText[3] = "Correct answer: In n Out",
-    qText[4] = "Correct answer: KFC"
-
-function isTrue() {
-    var trueFalse;
-    if (rightChoices) {
-        trueFalse = true;
-    } else {
-        trueFalse = false;
-    }
-}
+var qText = [
+    "Correct answer: Burger King",
+    "Correct answer: Mcdonald's",
+    "Correct answer: Taco Bell",
+    "Correct answer: In n Out",
+    "Correct answer: KFC"
+];
+// function isTrue() {
+//     var trueFalse;
+//     if (rightChoices) {
+//         trueFalse = true;
+//     } else {
+//         trueFalse = false;
+//     }
+// }
 //Event listeners for buttons
 startBtn.addEventListener("click", function () {
 });
 resetBtn.addEventListener("click", function () {
 });
 
-document.getElementById(".cbtn").addEventListener("click", function () {
-});
 //Hide resetBtn on load
 $("#resetBtn").hide();
 $("#countDisplay").hide();
@@ -107,6 +81,7 @@ function clearScreen() {
     $("#gifDiv").empty();
     $("#triviaQuestion").empty();
     $("#resetButton").hide();
+    $("#userChoices").empty();
 }
 //Start the timer
 function startTimer() {
@@ -123,7 +98,6 @@ function startTimer() {
         }, 1000);
     }
 }
-var restartTimer;
 //Time out
 function timeout() {
     clearInterval(countdownTimer);
@@ -134,53 +108,181 @@ function timeout() {
     showCorrect();
     $("#triviaQuestion").empty();
     unanswered = unanswered + 1;
+    setTimeout(clearScreen, 3000)
+    setTimeout(nextQ, 3500)
 }
-function nextQ() {
-
-}
-
+q1 = true;
 function startGame() {
     startTimer();
-    displayInfo();
+    renderQuestion();
     $("#startButton").hide();
+    renderButtons();
 
 }
-function displayInfo() {
-    for (var i = 0; i < 5; i++) {
-        $("#triviaQuestion").html(questionArray[i]);
-        $("#userChoices").html(choicesArray[i]);
+//Display question
+function renderQuestion() {
+    if (q1 === true) {
+        $("#triviaQuestion").html(questionArray[0]);
     }
-
-
-    // for (var i = 0; i < 4; i++) {
-    //     var choiceBtn = document.createElement("button");
-    //     choiceBtn.className = "choiceBtn" + i;
-    //     userChoices.appendChild(choiceBtn);
-    //     choiceBtn.innerHTML = choicesArray[i][i];
+    if (q2 === true) {
+        $("#triviaQuestion").html(questionArray[1]);
+    }
+    if (q3 === true) {
+        $("#triviaQuestion").html(questionArray[2]);
+    }
+    if (q4 === true) {
+        $("#triviaQuestion").html(questionArray[3]);
+    }
+    if (q5 === true) {
+        $("#triviaQuestion").html(questionArray[4]);
+    }
+}
+//Display next question
+function nextQ() {
+    renderQuestion();
+    renderButtons();
 
 }
-
+//Show div and text for correct answer
 function showCorrect() {
-    for (var i = 0; i < 5; i++) {
-        qText(i);
-        qGifs(i);
-
+    if (q1 === false && q2 === true) {
+        $('#gifDiv').html('<img src="assets/images/burgerkinggif.webp" />')
+        $("#triviaQuestion").html(qText[0])
     }
+    if (q2 === true) {
+        $('#gifDiv').html('<img src="assets/images/mcdonaldsgif.webp" />')
+        $("#triviaQuestion").html(qText[1])
+    }
+    if (q3 === true) {
+        $('#gifDiv').html('<img src="assets/images/tacoBellgif.webp" />')
+        $("#triviaQuestion").html(qText[2])
+    }
+    if (q4 === true) {
+        $('#gifDiv').html('<img src="assets/images/inNoutgif.webp" />')
+        $("#triviaQuestion").html(qText[3])
+    }
+    if (q5 === true) {
+        $('#gifDiv').html('<img src="assets/images/kfcgif.webp" />')
+        $("#triviaQuestion").html(qText[4])
+    }
+}
+//Append corresponding buttons to page
+function renderButtons() {
+
+    for (var i = 0; i < 4; i++) {
+        if (q1 === true) {
+            $("#userChoices").append(choicesArray[0][i])
+        }
+        if (q2 === true) {
+            $("#userChoices").append(choicesArray[1][i])
+        }
+        if (q3 === true) {
+            $("#userChoices").append(choicesArray[2][i])
+        }
+        if (q4 === true) {
+            $("#userChoices").append(choicesArray[3][i])
+        }
+        if (q5 === true) {
+            $("#userChoices").append(choicesArray[4][i])
+        }
+    }
+    $("#r").on('click', function () {
+        console.log("correct: " + correctAnswers)
+        correctAnswers++;
+    });
+
+    $(".cbtn").on('click', function () {
+        console.log('clicked');
+        clearInterval(countdownTimer);
+        clockRunning = false;
+        showCorrect();
+        q1 = false;
+        q2 = true;
+        $("#userChoices").empty();
+        setTimeout(clearScreen, 3000)
+        setTimeout(nextQ, 3500)
+        // if ($("div").attr("id") == "r") {
+        //     correctAnswers++;
+        // } else {
+        //     incorrectAnswers++;
+        // }
+        console.log("Incorrect:" + incorrectAnswers);
+    });
+    $(".cbtn2").on('click', function () {
+        console.log('clicked');
+        clearInterval(countdownTimer);
+        clockRunning = false;
+        showCorrect();
+        q2 = false;
+        q3 = true;
+        $("#userChoices").empty();
+        setTimeout(clearScreen, 3000)
+        setTimeout(nextQ, 3500)
+        // if ($("div").attr("id") == "r") {
+        //     correctAnswers++;
+        // } else {
+        //     incorrectAnswers++;
+        // }
+        console.log("Incorrect:" + incorrectAnswers);
+
+    });
+    $(".cbtn3").on('click', function () {
+        console.log('clicked');
+        clearInterval(countdownTimer);
+        clockRunning = false;
+        showCorrect();
+        q3 = false;
+        q4 = true;
+        $("#userChoices").empty();
+        // if ($("div").attr("id") == "r") {
+        //     correctAnswers++;
+        // } else {
+        //     incorrectAnswers++;
+        // }
+        setTimeout(clearScreen, 3000)
+        setTimeout(nextQ, 3500)
+        console.log("Incorrect:" + incorrectAnswers);
+
+    });
+    $(".cbtn4").on('click', function () {
+        console.log('clicked');
+        clearInterval(countdownTimer);
+        clockRunning = false;
+        showCorrect();
+        q4 = false;
+        q5 = true;
+        $("#userChoices").empty();
+        // if ($("div").attr("id") == "r") {
+        //     correctAnswers++;
+        // } else {
+        //     incorrectAnswers++;
+        // }
+        setTimeout(clearScreen, 3000)
+        setTimeout(nextQ, 3500)
+        console.log("Incorrect:" + incorrectAnswers);
+    });
+    $(".cbtn5").on('click', function () {
+        console.log('clicked');
+        clearInterval(countdownTimer);
+        clockRunning = false;
+        showCorrect();
+        q5 = true;
+        $("#userChoices").empty();
+        // if ($("div").attr("id") == "r") {
+        //     correctAnswers++;
+        // } else {
+        //     incorrectAnswers++;
+        // }
+        console.log("Incorrect:" + incorrectAnswers);
+
+    });
+
 }
 //Start the game
 $(startBtn).on("click", function () {
     startGame();
-
 });
 
-$(".cbtn").on("click", function () {
-    // if(trueFalse === true) {
-    //     alert("yes");
-    // } else if(trueFalse === false) {
-    //     alert("no");
-    // }
-    console.log();
-});
 
 
 
