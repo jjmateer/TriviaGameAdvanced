74b5c1564e7edbe985c1d50fbac6a6e10c7af900
John Mateer <jj.mateer@live.com>
2019-08-02 23:37:13 -0700

    Added a separate on click function for correct choices that use the zeroOne variable to determine if the answer is correct or incorrect. Set timeouts on clearscreen, nextq, and starttimer after each question to make the onclick function go to the next question after 3 seconds and restart the timer. Made a second function for timeouts and called it with if statements. If question 1-4, it will go to the next question and if question 5 it will go to the end screen where the correctAnswers, incorrectAnswers, and unanswered will be displayed.Currently, when the timer runs out of time, it start over and display the same question.

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -22,11 +22,11 @@ console.log(questionArray);
 //Choices
 
 var choicesArray = new Array();
-choicesArray[0] = ["<btn id='r' class='cbtn'>burgerKing</btn>", "<btn id='w' class='cbtn'>carlsJr</btn>", "<btn id='w' class='cbtn'>mcdonalds</btn>", "<btn id='w' class='cbtn'>tacoBell</btn>"];
-choicesArray[1] = ["<btn id='r' class='cbtn2'>mcdonalds</btn>", "<btn id='w' class='cbtn2'>tacoBell</btn>", "<btn id='w' class='cbtn2'>burgerKing</btn>", "<btn id='w' class='cbtn2'>carlsJr</btn>"];
-choicesArray[2] = ["<btn id='w' class='cbtn3'>burgerKing</btn>", "<btn id='w' class='cbtn3'>carlsJr</btn>", "<btn id='w' class='cbtn3'>mcdonalds</btn>", "<btn id='r' class='cbtn3'>tacoBell</btn>"];
-choicesArray[3] = ["<btn id='r' class='cbtn4'>inNOut</btn>", "<btn id='w' class='cbtn4'>burgerKing</btn>", "<btn id='w' class='cbtn4'>carlsJr</btn>", "<btn id='w' class='cbtn4'>tacoBell</btn>"];
-choicesArray[4] = ["<btn id='w' class='cbtn5'>carlsJr</btn>", "<btn id='w' class='cbtn5'>mcdonalds</btn>", "<btn id='r' class='cbtn5'>kfc</btn>", "<btn id='w' class='cbtn5'>tacoBell</btn>"];
+choicesArray[0] = ["<btn id='r' class='cbtn'>burgerKing</btn>", "<btn class='cbtn'>carlsJr</btn>", "<btn class='cbtn'>mcdonalds</btn>", "<btn class='cbtn'>tacoBell</btn>"];
+choicesArray[1] = ["<btn id='r' class='cbtn2'>mcdonalds</btn>", "<btn class='cbtn2'>tacoBell</btn>", "<btn class='cbtn2'>burgerKing</btn>", "<btn class='cbtn2'>carlsJr</btn>"];
+choicesArray[2] = ["<btn class='cbtn3'>burgerKing</btn>", "<btn class='cbtn3'>carlsJr</btn>", "<btn class='cbtn3'>mcdonalds</btn>", "<btn id='r' class='cbtn3'>tacoBell</btn>"];
+choicesArray[3] = ["<btn id='r' class='cbtn4'>inNOut</btn>", "<btn class='cbtn4'>burgerKing</btn>", "<btn class='cbtn4'>carlsJr</btn>", "<btn class='cbtn4'>tacoBell</btn>"];
+choicesArray[4] = ["<btn class='cbtn5'>carlsJr</btn>", "<btn class='cbtn5'>mcdonalds</btn>", "<btn id='r' class='cbtn5'>kfc</btn>", "<btn class='cbtn5'>tacoBell</btn>"];
 
 // Correct  choices
 var rightChoices = [
@@ -56,14 +56,6 @@ var qText = [
     "Correct answer: In n Out",
     "Correct answer: KFC"
 ];
-// function isTrue() {
-//     var trueFalse;
-//     if (rightChoices) {
-//         trueFalse = true;
-//     } else {
-//         trueFalse = false;
-//     }
-// }
 //Event listeners for buttons
 startBtn.addEventListener("click", function () {
 });
@@ -98,24 +90,43 @@ function startTimer() {
         }, 1000);
     }
 }
-//Time out
-function timeout() {
-    clearInterval(countdownTimer);
+function timeoutFunction() {
     $("#startButton").text("Time out!!");
-    $("#userChoices").hide();
-    clockRunning = false;
-    setTimeout(startTimer, 3000)
-    showCorrect();
-    $("#triviaQuestion").empty();
-    unanswered = unanswered + 1;
+    $("#userChoices").empty();
     setTimeout(clearScreen, 3000)
     setTimeout(nextQ, 3500)
+    setTimeout(startTimer, 3500)
+    unanswered++;
+    showCorrect();
+    clearInterval(countdownTimer);
+    clockRunning = false;
+    
+}
+//Time out
+function timeout() {
+
+    if (i <= -1 && q1 === true) {
+        timeoutFunction();
+    }
+    if (i <= -1 && q2 === true) {
+        timeoutFunction();
+    }
+    if (i <= -1 && q3 === true) {
+        timeoutFunction();
+    }
+    if (i <= -1 && q4 === true) {
+        timeoutFunction();
+    }
+    if (q5 === true) {
+        endGameScreen();
+        $("#timeRemaining").empty();
+    }
 }
 q1 = true;
 function startGame() {
     startTimer();
     renderQuestion();
-    $("#startButton").hide();
+    $("#startButton").empty();
     renderButtons();
 
 }
@@ -145,7 +156,7 @@ function nextQ() {
 }
 //Show div and text for correct answer
 function showCorrect() {
-    if (q1 === false && q2 === true) {
+    if (q1 === true) {
         $('#gifDiv').html('<img src="assets/images/burgerkinggif.webp" />')
         $("#triviaQuestion").html(qText[0])
     }
@@ -188,6 +199,22 @@ function renderButtons() {
     }
     $("#r").on('click', function () {
         console.log("correct: " + correctAnswers)
+        zeroOne = 1;
+        if(zeroOne === 1){
+            correctAnswers++;
+        }
+        console.log("01:" + zeroOne)
+    });
+    var zeroOne = 0;
+    if(zeroOne)
+    var rightChoices = [
+        choicesArray[0][0],
+        choicesArray[1][0],
+        choicesArray[2][3],
+        choicesArray[3][0],
+        choicesArray[4][2]
+    ];
+    $(rightChoices).on('click', function() {
         correctAnswers++;
     });
 
@@ -201,11 +228,7 @@ function renderButtons() {
         $("#userChoices").empty();
         setTimeout(clearScreen, 3000)
         setTimeout(nextQ, 3500)
-        // if ($("div").attr("id") == "r") {
-        //     correctAnswers++;
-        // } else {
-        //     incorrectAnswers++;
-        // }
+        setTimeout(startTimer, 3500)
         console.log("Incorrect:" + incorrectAnswers);
     });
     $(".cbtn2").on('click', function () {
@@ -218,13 +241,8 @@ function renderButtons() {
         $("#userChoices").empty();
         setTimeout(clearScreen, 3000)
         setTimeout(nextQ, 3500)
-        // if ($("div").attr("id") == "r") {
-        //     correctAnswers++;
-        // } else {
-        //     incorrectAnswers++;
-        // }
+        setTimeout(startTimer, 3500)
         console.log("Incorrect:" + incorrectAnswers);
-
     });
     $(".cbtn3").on('click', function () {
         console.log('clicked');
@@ -234,13 +252,9 @@ function renderButtons() {
         q3 = false;
         q4 = true;
         $("#userChoices").empty();
-        // if ($("div").attr("id") == "r") {
-        //     correctAnswers++;
-        // } else {
-        //     incorrectAnswers++;
-        // }
         setTimeout(clearScreen, 3000)
         setTimeout(nextQ, 3500)
+        setTimeout(startTimer, 3500)
         console.log("Incorrect:" + incorrectAnswers);
 
     });
@@ -252,13 +266,9 @@ function renderButtons() {
         q4 = false;
         q5 = true;
         $("#userChoices").empty();
-        // if ($("div").attr("id") == "r") {
-        //     correctAnswers++;
-        // } else {
-        //     incorrectAnswers++;
-        // }
         setTimeout(clearScreen, 3000)
         setTimeout(nextQ, 3500)
+        setTimeout(startTimer, 3500)
         console.log("Incorrect:" + incorrectAnswers);
     });
     $(".cbtn5").on('click', function () {
@@ -268,13 +278,8 @@ function renderButtons() {
         showCorrect();
         q5 = true;
         $("#userChoices").empty();
-        // if ($("div").attr("id") == "r") {
-        //     correctAnswers++;
-        // } else {
-        //     incorrectAnswers++;
-        // }
         console.log("Incorrect:" + incorrectAnswers);
-
+        setTimeout(endGameScreen, 3000)
     });
 
 }
@@ -295,6 +300,7 @@ function endGameScreen() {
         "Unanswered: " + unanswered);
     $("#resetBtn").show();
     $("#countDisplay").show();
+    $("#timeRemaining").hide();
     console.log(correctAnswers);
 }
 
